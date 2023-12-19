f1436c7c5ede2ad45a34555156dcc5b1a65c5f74
Max-munene <munenemaxwell2@gmail.com>
2023-12-19 15:50:06 +0300

    permalink

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-new-post',
-  templateUrl: './new-post.component.html',
-  styleUrls: ['./new-post.component.css']
+	selector: 'app-new-post',
+	templateUrl: './new-post.component.html',
+	styleUrls: ['./new-post.component.css'],
 })
 export class NewPostComponent {
-
+	permalink: string = '';
+	onTitleChanged($event: any) {
+		const title = $event.target.value;
+		this.permalink = title.replace(/\s+/g, '-');
+	}
 }
