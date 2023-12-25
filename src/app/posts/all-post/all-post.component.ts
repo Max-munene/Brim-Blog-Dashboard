776b0d25ce3c278e865c75efd971c2fe4e87f145
Max-munene <munenemaxwell2@gmail.com>
2023-12-25 21:07:48 +0300

    all-posts fetch-from firebase

--- src/app/posts/all-post/all-post.component.ts
+++ src/app/posts/all-post/all-post.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
-  selector: 'app-all-post',
-  templateUrl: './all-post.component.html',
-  styleUrls: ['./all-post.component.css']
+	selector: 'app-all-post',
+	templateUrl: './all-post.component.html',
+	styleUrls: ['./all-post.component.css'],
 })
 export class AllPostComponent {
+	postsArray!: Array<any>;
+	constructor(private postService: PostsService) {}
 
+	ngOnInit() {
+		this.postService.loadData().subscribe((val) => {
+			console.log(val);
+			this.postsArray = val;
+		});
+	}
 }
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { Route, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root',
@@ -11,6 +13,7 @@ export class PostsService {
 		private storage: AngularFireStorage,
 		private afs: AngularFirestore,
 		private toastr: ToastrService,
+		private router: Router,
 	) {}
 
 	uploadImage(selectedImage: any, postData: any) {
@@ -38,6 +41,23 @@ export class PostsService {
 			.then((docRef) => {
 				console.log(docRef);
 				this.toastr.success('Post added succesfully!');
+				this.router.navigate(['/posts']);
 			});
+	}
+
+	loadData() {
+		return this.afs
+			.collection('posts')
+			.snapshotChanges()
+			.pipe(
+				map((actions) => {
+					return actions.map((a) => {
+						const data = a.payload.doc.data();
+						const id = a.payload.doc.id;
+
+						return { id, data };
+					});
+				}),
+			);
 	}
 }
